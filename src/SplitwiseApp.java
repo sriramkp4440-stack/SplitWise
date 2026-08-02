12a2346ea91ffeb0fa5fa95c5796ad335e14784d
sriramkp4440-stack <sriram.25bsccsda@kprcas.ac.in>
2026-08-02 17:13:15 +0530

    1.1

---
+++ src/SplitwiseApp.java
@@ -0,0 +1,8 @@
+public class SplitwiseApp {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("===Splitwise Console===");
+        System.out.println("Track shared expenses with friends");
+        System.out.println();
+    }
+}
