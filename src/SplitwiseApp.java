3f3fb20aef8544ad03d177fb3a268335d054a361
sriramkp4440-stack <sriram.25bsccsda@kprcas.ac.in>
2026-08-02 18:20:55 +0530

    1.3

--- src/SplitwiseApp.java
+++ src/SplitwiseApp.java
@@ -4,9 +4,11 @@ public class SplitwiseApp {
         System.out.println("===Splitwise Console===");
         System.out.println("Track shared expenses with friends");
         System.out.println();
+        String payerName = "Riya";
         double totalAmount = 500;
         int numFriends = 3;
         System.out.println(totalAmount);
         System.out.println(numFriends);
+        System.out.println(payerName);
     }
 }
